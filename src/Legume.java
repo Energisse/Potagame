7fc38e4a65cd6afe4ad60e18cea7444f07524957
ThomasBlumet <thomas.blumet@orange.fr>
2023-05-22 09:46:13 +0200

    Création classes nécessaire

--- src/Legume.java
+++ src/Legume.java
@@ -1,7 +1,11 @@
 public class Legume {
-    enum Graines{SALADE,TOMATE,}
+    float tx_soleil ;
+    float tx_humidite ;
+    float temperature ;
+    enum Graines{SALADE,TOMATE,OIGNON,AIL,ECHALOTTE,POIVRON,NAVET,CHOUX,BETTERAVE,PATATE,CAROTTES}
+    enum Arbres_fruitier{PECHE,BANANIER,CERISIER}
     for (Graines g : Graines.values()){
-
+        System.out.println("comment");
     }
     private void croissance(){//la croissance prend en compte l'ensoleillement,la température et l'humidité
 
--- src/Vue.java
+++ src/Vue.java
@@ -1,14 +1,18 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Vue extends JFrame implements Observer { //JFrame=la fenetre; Jpanel=conteneur s'inserant dans la fenetre; Jlabel=contient texte/image
-
+    private string urlIconelegume = "D:/Cours Java/ProjetInfoEncadre/Potager/img/data.png"; //chemin de stockage des legumes
+    private string urlIconeterre = "D:/Cours Java/ProjetInfoEncadre/Potager/img/Terre.png"; //chemin de stockage de la terre
     private JPanel[][] tab ;
     private Modele m;
     public Vue(Modele m){
@@ -31,7 +35,12 @@ public class Vue extends JFrame implements Observer { //JFrame=la fenetre; Jpane
             for(int j=0; j<m.size_y ; j++){
                 tab[i][j]= new JPanel();
                 tab[i][j].setPreferredSize(new Dimension( 50, 50));
-                tab[i][j].setBackground( m.getCaseContent(i,j) ? Color.BLACK : Color.WHITE);
+                //tab[i][j].setBackground( m.getCaseContent(i,j) ? Color.BLACK : Color.WHITE);
+                BufferedImage image = ImageIO.read(new File(urlIconeterre)); //lecture du fichier d'image
+                BufferedImage img_place = image.getSubimage(10,10, m.size_x, m.size_y); // image du légume (x, y : coin supérieur gauche, w, h : largeur et hauteur)
+                ImageIcon icone = img_place.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH)); // icône redimentionnée
+                //tab[i][j].setIcon(icone); // partie rafraichissement
+            }
                 tab[i][j].setBorder(BorderFactory.createLineBorder(Color.YELLOW));
                 jpn.add(tab[i][j]);
 
@@ -60,10 +69,11 @@ public class Vue extends JFrame implements Observer { //JFrame=la fenetre; Jpane
             }
         }
     }
-    //urlIcone = "D:\Downloads\data.png"
+    //urlIcone = "D:\Cours Java\ProjetInfoEncadre\Potager\img\data.png"
     //BufferedImage image = ImageIO.read(new File(urlIcone)); // chargement de
     //l'image globale
     //
+    //new ImageIcon()
     //BufferedImage salade = image.getSubimage(x, y, w, h); // image du légume
     //le légume (x, y : coin supérieur gauche, w, h : largeur et hauteur)
     //
